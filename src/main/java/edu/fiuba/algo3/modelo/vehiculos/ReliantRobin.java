06dab38b841b6cd95aa26f422e823beb06c5d767
Emanuel Tello <etello@fi.uba.ar>
2022-07-21 17:48:03 -0300

    reliantRobin mejora

--- src/main/java/edu/fiuba/algo3/modelo/vehiculos/ReliantRobin.java
+++ src/main/java/edu/fiuba/algo3/modelo/vehiculos/ReliantRobin.java
@@ -3,17 +3,26 @@ package edu.fiuba.algo3.modelo.vehiculos;
 import edu.fiuba.algo3.modelo.modificador.*;
 import edu.fiuba.algo3.modelo.tablero.Tablero;
 
+import java.util.Random;
+
 public class ReliantRobin extends Vehiculo {
 
     private long penalizacionPorPozo = 3;
+
+    private long penalizacionPorPiquetazo = 2;
+
+    private Vehiculo vehiculoActual;
 
     private Auto auto = new Auto(this.tablero);
 
     private Moto moto = new Moto(this.tablero);
 
+    private float probaComportamiento = 0.6f;
+
     public ReliantRobin(Tablero tablero) {
         super(tablero);
         this.nombre = "reliantrobin";
+        this.vehiculoActual = auto;
     }
 
     @Override
@@ -29,12 +38,30 @@ public class ReliantRobin extends Vehiculo {
 
     @Override
     public void aplicarModificador(Piquete piquete) {
-        this.actualizarASiguienteCelda();
+        this.setVehiculoActual();
+
+        this.vehiculoActual.setDireccionActual(this.direccionActual);
+        this.vehiculoActual.asignarCeldaInicial(this.celdaInicial);
+        vehiculoActual.aplicarModificador(piquete);
+        this.setMovimientos(this.movimientos += vehiculoActual.movimientos());
+
+        if(this.celdaInicial != vehiculoActual.getPosicion()) {
+            this.actualizarASiguienteCelda();
+            vehiculoActual.setMovimientos(0);
+        }
     }
 
     @Override
     public void aplicarModificador(ControlPolicial controlPolicial) {
+        this.setVehiculoActual();
+
+        this.vehiculoActual.setDireccionActual(this.direccionActual);
+        this.vehiculoActual.asignarCeldaInicial(this.celdaInicial);
+        vehiculoActual.aplicarModificador(controlPolicial);
+        this.setMovimientos(this.movimientos += vehiculoActual.movimientos());
+
         this.actualizarASiguienteCelda();
+        vehiculoActual.setMovimientos(0);
     }
 
     @Override
@@ -51,6 +78,18 @@ public class ReliantRobin extends Vehiculo {
 
     @Override
     public void aplicarModificador(Piquetazo piquetazo) {
-        this.actualizarASiguienteCelda();
+        this.sumarMovimientos(this.penalizacionPorPiquetazo);
     }
+
+    public void setVehiculoActual() {
+        Random rand = makeRandom();
+        float f = rand.nextFloat();
+        if(f <= probaComportamiento) {
+            this.vehiculoActual = auto;
+        }
+        else {
+            this.vehiculoActual = moto;
+        }
+    }
+
 }
